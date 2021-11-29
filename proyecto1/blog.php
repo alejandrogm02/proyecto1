791a3a1a0e35f15b0d0480a0dc7cc917ee571b53
alejandrogm02 <78848296+alejandrogm02@users.noreply.github.com>
2021-11-29 10:35:25 +0100

    Delete proyecto1 directory

--- proyecto1/blog.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $title = "Blog";
-    require_once "./utils/utils.php";
-    include("./views/blog.view.php");
--- proyecto1/single_post.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $title = "Single post";
-    require_once "./utils/utils.php";
-    include("./views/single_post.view.php");
